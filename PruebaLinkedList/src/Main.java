52c60425d4b0e4582f84c6e7e1c24d6c0ada9cfa
08AdriBS <adrian800808@gmail.com>
2026-08-20 12:29:45 -0700

    added PruebaLinkedList.java

---
+++ PruebaLinkedList/src/Main.java
@@ -0,0 +1,46 @@
+
+// @author Adrian Briseño Sabori
+// @date Aug 17, 2026
+
+
+class Student {
+    String name;
+    int score;
+    Student next;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        LnkdLst lista = new LnkdLst();
+        lista.add("Obet mednes", 39);
+        lista.add("Ciret deshidratado", 1000);
+        lista.add("Duego alberno", 2);
+    }
+}
+
+
+class LnkdLst {
+    Student head;
+    public LnkdLst() {
+        this.head = null;
+    }
+
+    public void add(String name, int score) {
+        Student newStudent = new Student(name, score);
+        if (head == null) {
+            head = newStudent;
+            return;
+        }
+        Student current = head;
+        while(current.next != null) {
+            current = current.next;
+        }
+        current.next = newStudent;
+    }
+}
